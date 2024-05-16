56fc70d9c8988ec2ba68fad293df81b6fb4bc077
Volodymyr Basok <volodymyrquo@gmail.com>
2024-05-16 16:41:55 +0300

    Initial commit

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo, useCallback,useState, useRef, useEffect} from 'react';
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -6,20 +6,82 @@ import SimpleMDE from 'react-simplemde-editor';
 
 import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
+import { useSelector } from 'react-redux';
+import { selectIsAuth } from '../../redux/slices/auth';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import axios from '../../axios'
 
 export const AddPost = () => {
-  const imageUrl = '';
-  const [value, setValue] = React.useState('');
+  const {id} = useParams()
+  const navigate = useNavigate()
+  const isAuth = useSelector(selectIsAuth);
+const inputFileRef = useRef(null)
+const [isLoading, setIsLoading] = useState(false)
+  const [text, setText] = useState('');
+  const [title, setTitle] = useState('');
+  const [tags, setTags] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
-  const handleChangeFile = () => {};
+const isEditing = Boolean(id)
+  const handleChangeFile = async(event) => {
+try {
+  const formData = new FormData()
+  const file =  event.target.files[0];
+  formData.append('image',file)
+  const {data} = await axios.post('/upload',formData)
+  setImageUrl(data.url)
+} catch (error) {
+  console.warn(error);
+  alert('Error when file uploading')
+}
+  };
 
-  const onClickRemoveImage = () => {};
+  const onClickRemoveImage = () => {
+    setImageUrl('')
+  };
 
-  const onChange = React.useCallback((value) => {
-    setValue(value);
+  const onChange = useCallback((value) => {
+    setText(value);
   }, []);
 
-  const options = React.useMemo(
+  const onSubmit = async()=>{
+try {
+  setIsLoading(true);
+  const fields = {
+    title,
+    imageUrl,
+    tags,
+    text
+  }
+
+  const {data}=isEditing
+  ?await axios.patch(`/posts/${id}`,fields)
+  :await axios.post('/posts',fields)
+  const postId = isEditing? id: data._id
+  navigate(`/posts/${postId}`)
+} catch (error) {
+  console.warn(error)
+  alert("Error article creating")
+}
+  }
+
+  useEffect(()=>{
+    console.log({id})
+if(id){
+  axios.get(`/posts/${id}`)
+  .then(({data})=>{
+    setTitle(data.title);
+    setText(data.text);
+    setImageUrl(data.imageUrl);
+    setTags(data.tags.join(','));
+  })
+  .catch(err=>{
+    console.warn(err);
+  alert("Error edit article")
+  })
+}
+  },[id])
+  const options = useMemo(
     () => ({
       spellChecker: false,
       maxHeight: '400px',
@@ -33,20 +95,24 @@ export const AddPost = () => {
     }),
     [],
   );
-
+  if(!window.localStorage.getItem('token') && !isAuth){
+    return <Navigate to='/'/>
+  }
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
-        Загрузить превью
+      <Button onClick={()=>inputFileRef.current.click()} variant="outlined" size="large">
+     Load preview
       </Button>
-      <input type="file" onChange={handleChangeFile} hidden />
+      <input ref={inputFileRef} type="file" onChange={handleChangeFile} hidden  />
       {imageUrl && (
-        <Button variant="contained" color="error" onClick={onClickRemoveImage}>
+        <>  
+          <Button variant="contained" color="error" onClick={onClickRemoveImage}>
           Удалить
         </Button>
-      )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt="Uploaded" />
+        <img className={styles.image} src={`http://localhost:4444/${imageUrl}`} alt="Uploaded" />
+
+        </>
+
       )}
       <br />
       <br />
@@ -54,16 +120,18 @@ export const AddPost = () => {
         classes={{ root: styles.title }}
         variant="standard"
         placeholder="Заголовок статьи..."
+        value={title}
+        onChange={e=>setTitle(e.target.value)}
         fullWidth
       />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
-      <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
+      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" value={tags} onChange={e=>setTags(e.target.value)} fullWidth />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
       <div className={styles.buttons}>
-        <Button size="large" variant="contained">
-          Опубликовать
+        <Button onClick={onSubmit} size="large" variant="contained">
+          {isEditing?'Save':'Public'}
         </Button>
         <a href="/">
-          <Button size="large">Отмена</Button>
+          <Button size="large">Reject</Button>
         </a>
       </div>
     </Paper>
--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -6,22 +6,67 @@ import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 
 import styles from './Login.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import {  fetchRegister, selectIsAuth } from '../../redux/slices/auth';
+import { useForm } from 'react-hook-form';
+import { Navigate } from 'react-router-dom';
 
 export const Registration = () => {
+  const isAuth = useSelector(selectIsAuth)
+  const dispatch = useDispatch()
+  const {register, handleSubmit, setError, formState:{errors, isValid}} = useForm({
+    defaultValues:{
+      fullName:'',
+      email:'',
+      password:''
+    },
+    mode:'onChange'
+  })
+  const onSubmit = async (values)=>{
+    const data = await dispatch(fetchRegister(values))
+    if(!data.payload){
+     alert('Cannot register')
+   
+    }
+    if('token' in data.payload){
+     window.localStorage.setItem('token',data.payload.token)
+    } 
+     }
+   if(isAuth){
+     return <Navigate to='/'/>
+   }
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
-        Создание аккаунта
+       Create account
       </Typography>
       <div className={styles.avatar}>
         <Avatar sx={{ width: 100, height: 100 }} />
       </div>
-      <TextField className={styles.field} label="Полное имя" fullWidth />
-      <TextField className={styles.field} label="E-Mail" fullWidth />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Зарегистрироваться
+      <form onSubmit={handleSubmit(onSubmit)}>
+              <TextField className={styles.field} label="Full name"
+              {...register('fullName',{required:'Add full name'})}
+       helperText={errors.fullName?.message}
+       error={Boolean(errors.fullName?.message)}
+
+      fullWidth />
+      <TextField className={styles.field} label="E-Mail"
+              {...register('email',{required:'Add email'})}
+       helperText={errors.email?.message}
+       error={Boolean(errors.email?.message)}
+type='email'
+      fullWidth />
+      <TextField className={styles.field}
+            {...register('password',{required:'Add password'})}
+       helperText={errors.password?.message}
+       error={Boolean(errors.password?.message)}
+type='password'
+      label="Password" fullWidth />
+      <Button type='submit' size="large" variant="contained" disabled={!isValid} fullWidth>
+        Register
       </Button>
+
+      </form>
     </Paper>
   );
 };
